ae4a0c683793062a6b36946dbb42beaefde0fb31
Njaaga-Gakure <njaagagakure@gmail.com>
2023-08-11 04:38:05 +0000

    Write a function that diveds two integers

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,20 @@
+-- create a function that divides two ints
+DELIMITER // 
+
+CREATE FUNCTION SafeDiv (
+	a INT,
+	b INT
+)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+	IF b = 0 THEN
+		RETURN 0;
+	END IF;
+
+	RETURN a / b;
+END;
+
+// 
+
+DELIMITER ;
